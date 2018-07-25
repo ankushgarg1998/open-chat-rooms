187e8bec8d5703bc0d5738ed0623363bee1d9b81
ankushgarg1998 <1998anky@gmail.com>
2018-07-25 23:01:05 +0530

    Added messaging working

--- public/js/index.js
+++ public/js/index.js
@@ -10,11 +10,19 @@ socket.on('disconnect', function () {
 
 socket.on('newMessage', function (message) {
     console.log('New Message', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
 });
 
-socket.emit('createMessage', {
-    from: 'Frank',
-    text: 'Hi'
-}, function(text) {
-    console.log(text);
+jQuery('#message-form').on('submit', function (e) {
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message-form]').val()
+    }, function () {
+
+    });
 });

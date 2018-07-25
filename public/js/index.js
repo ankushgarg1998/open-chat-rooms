5a592cb6b093bd081c8a11d2928ed197eaf88569
ankushgarg1998 <1998anky@gmail.com>
2018-07-25 22:03:14 +0530

    Alerts for new joining users.

---
+++ public/js/index.js
@@ -0,0 +1,13 @@
+var socket = io();
+
+socket.on('connect', function () {
+    console.log('Connected to server');
+});
+
+socket.on('disconnect', function () {
+    console.log('Disconnected from server');
+});
+
+socket.on('newMessage', function (message) {
+    console.log('New Message', message);
+});
--- server/server.js
+++ server/server.js
@@ -14,6 +14,24 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
     console.log('New user connected');
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the Node-Chat-App',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: `New User joined`,
+        createdAt: new Date().getTime()
+    })
+
+    socket.on('createMessage', (message) => {
+        console.log('Create Message', message);
+        message.createdAt = new Date().getTime();
+        io.emit('newMessage', message);
+    });
+
     socket.on('disconnect', () => {
         console.log('User was Disconnected');
     });

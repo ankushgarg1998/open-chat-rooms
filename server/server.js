70fe67fe10501f77ba887b814cd915bdd4c060e8
ankushgarg1998 <1998anky@gmail.com>
2018-07-26 00:21:31 +0530

    Added send Location

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
-const {generateMessage} = require('./utils/message');
+const {generateMessage, generateLocationMessage} = require('./utils/message');
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 var app = express();
@@ -22,6 +22,10 @@ io.on('connection', (socket) => {
         console.log('Create Message', message);
         io.emit('newMessage', generateMessage(message.from, message.text));
         callback('This is from server');
+    });
+
+    socket.on('createLocationMessage', (coords) => {
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
     });
 
     
